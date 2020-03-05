53067441df555a70faee5d994e16701a267ad3f2
lcy <251007996@qq.com>
2020-03-05 15:18:49 +0800

    1

---
+++ src/main/java/com/lcy/App.java
@@ -0,0 +1,13 @@
+package com.lcy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
