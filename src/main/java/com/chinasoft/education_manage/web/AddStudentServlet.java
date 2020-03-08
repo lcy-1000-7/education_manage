c1e27ef2924e1e9040eada248e9c832b88f4b516
lcy <251007996@qq.com>
2020-03-08 12:36:24 +0800

    lcy

--- src/main/java/com/chinasoft/education_manage/web/AddStudentServlet.java
+++ src/main/java/com/chinasoft/education_manage/web/AddStudentServlet.java
@@ -1,13 +1,10 @@
 package com.chinasoft.education_manage.web;
 
-<<<<<<< HEAD
 import com.chinasoft.education_manage.domain.Student;
 import com.chinasoft.education_manage.service.StudentService;
 import com.chinasoft.education_manage.service.impl.StudentServiceImpl;
 import com.chinasoft.education_manage.utils.StudentUtils;
 
-=======
->>>>>>> 6b002d26b49f6047317ddbecd5c0bcf6f33a3057
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,21 +16,13 @@ import java.io.IOException;
 public class AddStudentServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("utf-8");
-<<<<<<< HEAD
         Student student = StudentUtils.populate(new Student(),request.getParameterMap());
         StudentService studentService = new StudentServiceImpl();
         studentService.addStudent(student);
         response.sendRedirect(request.getContextPath()+"/studentMessageServlet?pageNum=1&rows=5");
-=======
-        Student student =
->>>>>>> 6b002d26b49f6047317ddbecd5c0bcf6f33a3057
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         this.doPost(request, response);
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 6b002d26b49f6047317ddbecd5c0bcf6f33a3057
